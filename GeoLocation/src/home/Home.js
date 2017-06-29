74250d245b7f16a0f05f287ee4a201030a4dbe48
Sandisile Mtileni <mtilenbruce@gmail.com>
2017-06-29 15:21:09 +0200

    changes made

--- GeoLocation/src/home/Home.js
+++ GeoLocation/src/home/Home.js
@@ -3,14 +3,16 @@ import MapView from 'react-native-maps';
 
 class UserGeoLocation extends Component {
 
-
+    //initials state with current position
     state = {
         currentPosition: 'unknown',
     }
 
+    //check watch  ID
     watchID: ? number = null;
 
     componentDidMount = ()⇒ {
+    	//get current position
         navigator.geolocation.getCurrentPosition(
             (position)⇒ {
                 const currentPosition = JSON.stringify(position);
@@ -18,7 +20,7 @@ class UserGeoLocation extends Component {
             },
             (error)⇒ alert(error.message), { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
         );
-
+	// watch Position as the user moves
         this.watchID = navigator.geolocation.watchPosition((position)⇒ {
             const currentPosition = JSON.stringify(position);
             this.setState({ currentPosition });
@@ -26,11 +28,14 @@ class UserGeoLocation extends Component {
     }
 
     componentWillUnmount = ()⇒ {
+    	// Clear Watch
         navigator.geolocation.clearWatch(this.watchID);
     }
+    //render View
     render() {
+
         return ( 
-            <View style = { styles.container } >
+            <View style = { styles.container }>
             <MapView style = { styles.map }
             currentRegion = {
                 {
@@ -44,8 +49,24 @@ class UserGeoLocation extends Component {
             }
 
             />  
-            </MapView > 
+            </MapView> 
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+   container: {
+   	flex:1,
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: '#F5FCFF',
+    },
+    map: {
+     position: 'absolute',
+     top:0,
+     left:0,
+     right:0,
+     bottom:0,
+     }
+    });
